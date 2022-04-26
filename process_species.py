9924b675498ee66b602f0cdffdf6b551454658b7
michaelyhuang23 <michaelyhuang23@gmail.com>
2022-04-26 05:31:30 -0400

    processing

--- process_species.py
+++ process_species.py
@@ -9,7 +9,7 @@ with open('countries.txt', 'r') as f:
 	countries = [country[:-1].lower() for country in countries]
 
 data = pd.read_csv("all-mammals.csv", encoding="ISO-8859-1")
-data = data[['Family','Genus','Species','CommonName','TypeLocality']]
+data = data[['Order', 'Family','Genus','Species','CommonName','TypeLocality']]
 print(data.columns)
 
 
@@ -25,6 +25,7 @@ for index, row in data.iterrows():
 	if not containCountry : continue
 	print(row)
 	if not isinstance(row['CommonName'],str) : continue
+	if not isinstance(row['Order'],str) : continue
 	if not isinstance(row['Family'],str) : continue
 	if not isinstance(row['Genus'],str) : continue
 	if not isinstance(row['Species'],str) : continue
@@ -32,9 +33,14 @@ for index, row in data.iterrows():
 
 data = data.iloc[keep_indices]
 
+data['Order'] = data['Order'].str.lower()
 data['Family'] = data['Family'].str.lower()
 data['Genus'] = data['Genus'].str.lower()
 data['Species'] = data['Species'].str.lower()
+
+with open('UniqueOrder.txt', 'w') as f:
+	orderList = [order+'\n' for order in data['Order'].unique()]
+	f.writelines(orderList)
 
 with open('UniqueFamily.txt', 'w') as f:
 	familyList = [family+'\n' for family in data['Family'].unique()]
@@ -49,9 +55,17 @@ with open('UniqueSpecies.txt', 'w') as f:
 	f.writelines(speciesList)
 
 
-print(data['Family'].unique())
-
 data.to_csv("good-mammals.csv")
+print(len(data))
 
+with open('good-mammals.txt', 'w') as f:
+	for index, row in data.iterrows():
+		order = row['Order']
+		family = row['Family']
+		genus = row['Genus']
+		species = row['Species']
+		commonName = row['CommonName']
+		habitat = row['TypeLocality']
+		f.write(f'{order}#{family}#{genus}#{species}#{commonName}#{habitat}\n')
 
 
